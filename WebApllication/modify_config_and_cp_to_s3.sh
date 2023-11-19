9609fa6598262e99aef3ee235cd468873804b10d
Petri Panula <ext-petri.panula@traficom.fi>
2023-11-19 21:21:04 +0200

    Testing

--- WebApllication/modify_config_and_cp_to_s3.sh
+++ WebApllication/modify_config_and_cp_to_s3.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cp js/config.org js/config.temp
-sed  -i "s/xxxx/$(sam list stack-outputs --stack-name weasel-sam-demo-stack |grep CognitoPoolID | awk '{print $2}')/" js/config.temp
-sed  -i "s/yyyy/$(sam list stack-outputs --stack-name weasel-sam-demo-stack |grep CognitoPoolAppID | awk '{print $2}')/" js/config.temp
-OUTPUT=$(sam list stack-outputs --stack-name weasel-sam-demo-stack --output json |grep api  | grep -o -P '(?<=: ").*(?=/")')
+sed  -i "s/xxxx/$(sam list stack-outputs --stack-name sam-app-dev |grep CognitoPoolID | awk '{print $2}')/" js/config.temp
+sed  -i "s/yyyy/$(sam list stack-outputs --stack-name sam-app-dev |grep CognitoPoolAppID | awk '{print $2}')/" js/config.temp
+OUTPUT=$(sam list stack-outputs --stack-name sam-app-dev --output json |grep api  | grep -o -P '(?<=: ").*(?=/")')
 echo "${OUTPUT}"
 sed -i "s|zzzz|$OUTPUT|" js/config.temp
 cp js/config.temp js/config.js
