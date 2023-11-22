27ef0b2a4fb2a69432ae9e839a8f2a95e5ee0982
Petri Panula <ext-petri.panula@traficom.fi>
2023-11-22 15:08:20 +0200

    Testing

--- WebApllication/js/config.temp
+++ WebApllication/js/config.temp
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-north-1_T9NC3kT8k', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '60i31pbfieq88se518har10gf3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-north-1_gtaXKp8CP', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4omgiagg5i4mbigvk86bc94i63', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://wc578dyi02.execute-api.eu-north-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://w7e7k705qe.execute-api.eu-north-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
--- WebApllication/modify_config_and_cp_to_s3.sh
+++ WebApllication/modify_config_and_cp_to_s3.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cp js/config.org js/config.temp
-sed  -i "s/xxxx/$(sam list stack-outputs --stack-name sam-app-dev |grep CognitoPoolID | awk '{print $2}')/" js/config.temp
-sed  -i "s/yyyy/$(sam list stack-outputs --stack-name sam-app-dev |grep CognitoPoolAppID | awk '{print $2}')/" js/config.temp
-OUTPUT=$(sam list stack-outputs --stack-name sam-app-dev --output json |grep api  | grep -o -P '(?<=: ").*(?=/")')
+sed  -i "s/xxxx/$(sam list stack-outputs --stack-name ${ENV_STACK_NAME} |grep CognitoPoolID | awk '{print $2}')/" js/config.temp
+sed  -i "s/yyyy/$(sam list stack-outputs --stack-name ${ENV_STACK_NAME} |grep CognitoPoolAppID | awk '{print $2}')/" js/config.temp
+OUTPUT=$(sam list stack-outputs --stack-name ${ENV_STACK_NAME} --output json |grep api  | grep -o -P '(?<=: ").*(?=/")')
 echo "${OUTPUT}"
 sed -i "s|zzzz|$OUTPUT|" js/config.temp
 cp js/config.temp js/config.js
-aws s3 cp ./ s3://pete.weasel.fi --recursive
+aws s3 cp ./ s3://${ENV_STACK_NAME} --recursive
