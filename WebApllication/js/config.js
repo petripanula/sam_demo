f6319ff6ba22aac7978888eb32a9576071be0241
Petri Panula <ext-petri.panula@traficom.fi>
2023-11-22 10:48:16 +0200

    testing

--- WebApllication/js/config.js
+++ WebApllication/js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-north-1_vvxjQ9kWA', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '556rinp4j47kkme1q7553lkcr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-north-1_T9NC3kT8k', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '60i31pbfieq88se518har10gf3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://wm3dd0v0gb.execute-api.eu-north-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://wc578dyi02.execute-api.eu-north-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
--- create.sh
+++ create.sh
@@ -5,7 +5,7 @@ aws s3 cp ./index.zip s3://weasel-sam-test
 cd ..
 sam build
 sam validate
-sam deploy --capabilities CAPABILITY_IAM --stack-name sam-app-dev
+sam deploy --capabilities CAPABILITY_IAM --resolve-s3 --stack-name sam-app-dev
 cd WebApllication/
 ./modify_config_and_cp_to_s3.sh
 
