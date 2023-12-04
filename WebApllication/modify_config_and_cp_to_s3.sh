29037ba425fbbf39e24061927bd5f6476cf72a18
Petri Panula <ext-petri.panula@traficom.fi>
2023-12-04 11:59:46 +0200

    Testing

--- WebApllication/modify_config_and_cp_to_s3.sh
+++ WebApllication/modify_config_and_cp_to_s3.sh
@@ -3,8 +3,6 @@
 cp js/config.org js/config.temp
 sed  -i "s/xxxx/$(sam list stack-outputs --stack-name ${ENV_STACK_NAME} |grep CognitoPoolID | awk '{print $2}')/" js/config.temp
 sed  -i "s/yyyy/$(sam list stack-outputs --stack-name ${ENV_STACK_NAME} |grep CognitoPoolAppID | awk '{print $2}')/" js/config.temp
-OUTPUT=$(sam list stack-outputs --stack-name ${ENV_STACK_NAME} --output json |grep api  | grep -o -P '(?<=: ").*(?=/")')
-echo "${OUTPUT}"
-sed -i "s|zzzz|$OUTPUT|" js/config.temp
+sed  -i "s/zzzz/$(sam list stack-outputs --stack-name ${ENV_STACK_NAME} |grep WebsiteUrl | grep -o -P '(?<=//).*(?=.pete)')/g" js/config.temp
 cp js/config.temp js/config.js
 aws s3 cp ./ s3://${ENV_STACK_NAME} --recursive
